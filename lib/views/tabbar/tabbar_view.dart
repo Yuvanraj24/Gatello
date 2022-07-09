199b1bcc2d5387ea3b8de5f138b201a084ae3f5a
Yuvanraj24 <itsmeyuvanraj@gmail.com>
2022-07-08 23:15:45 -0700

    login

---
+++ lib/views/tabbar/tabbar_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Tabbar extends StatefulWidget {
+  const Tabbar({Key? key}) : super(key: key);
+
+  @override
+  State<Tabbar> createState() => _TabbarState();
+}
+
+class _TabbarState extends State<Tabbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
