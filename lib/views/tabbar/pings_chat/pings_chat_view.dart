7453263a7d0d251536e5cd3979f2b945ebf987f2
Yuvanraj24 <itsmeyuvanraj@gmail.com>
2022-07-24 18:59:31 +0545

    Yuvan Push

--- lib/views/tabbar/pings_chat/pings_chat_view.dart
+++ lib/views/tabbar/pings_chat/pings_chat_view.dart
@@ -5,6 +5,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gatello/core/models/pings_chat_model/pings_chats_list_model.dart';
 import 'package:gatello/views/tabbar/chats/pesrsonal_chat.dart';
+import 'package:gatello/views/tabbar/chats/selected_contact.dart';
 
 
 import 'package:google_fonts/google_fonts.dart';
@@ -24,8 +25,10 @@ class _PingsChatViewState extends State<PingsChatView> {
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   String? uid;
   List<PingsChatListModel> tileData = [];
-  final _isSelected = Map();
+  final _isSelected = [];
   bool selects = false;
+  bool change = false;
+
   @override
   void initState(){
     super.initState();
@@ -47,136 +50,18 @@ class _PingsChatViewState extends State<PingsChatView> {
     return Scaffold(
 
       backgroundColor: Color.fromRGBO(26, 52, 130, 0.06),
-      // body: change == true
-      //     ? Container(
-      //         padding: EdgeInsets.only(top: 110),
-      //         child: Center(
-      //           child: Column(
-      //             children: [
-      //               Image.asset(
-      //                   "assets/tabbar_icons/chats_image/chats_empty.png"),
-      //               Text("No Conversation",
-      //                   style: GoogleFonts.raleway(
-      //                       textStyle: TextStyle(
-      //                           color: Color.fromRGBO(0, 0, 0, 1),
-      //                           fontSize: 22,
-      //                           fontWeight: FontWeight.w700))),
-      //               Text(
-      //                 "You don't made any conversation yet",
-      //                 style: GoogleFonts.raleway(
-      //                     color: Color.fromRGBO(122, 122, 122, 1),
-      //                     fontSize: 14),
-      //               ),
-      //             ],
-      //           ),
-      //         ))
-      //     : Container(
-      //         color: Color.fromRGBO(26, 52, 130, 0.06),
-      //         child: ListView.builder(
-      //           itemCount: tileData.length,
-      //           itemBuilder: (context, index) {
-      //             return Padding(
-      //               padding: const EdgeInsets.only(bottom: 2),
-      //               child: InkWell(
-      //                 onTap: () {},
-      //                 onLongPress: () {},
-      //                 child: Container(
-      //                   // color: (tileData.contains(index))
-      //                   //                   ? Colors.blue.withOpacity(0.5)
-      //                   //                   : Colors.transparent,
-      //
-      //                   child: ListTile(
-      //                     onLongPress: () {
-      //                       toggleSelection();
-      //                     },
-      //                     onTap: () {u
-      //                       if (tileData.contains(index)) {
-      //                         setState(() {
-      //                           tileData.removeWhere((val) => val == index);
-      //                         });
-      //                       } else {
-      //                         Navigator.push(
-      //                             context,
-      //                             MaterialPageRoute(
-      //                                 builder: (context) => PersonalChat()));
-      //                       }
-      //                     },
-      //                     tileColor: Colors.white,
-      //                     contentPadding: EdgeInsets.only(
-      //                         left: 10, right: 10, top: 6, bottom: 6),
-      //                     //  contentPadding: EdgeInsets.all(10),
-      //                     leading: CircleAvatar(
-      //                       radius: 25.5.h,
-      //                       backgroundImage: NetworkImage(tileData[index].dp),
-      //                     ),
-      //                     selected: isSelected,
-      //
-      //                     title: Text(
-      //                       tileData[index].name,
-      //                       style: GoogleFonts.inter(
-      //                           textStyle: TextStyle(
-      //                               fontSize: 16.sp,
-      //                               color: Color.fromRGBO(0, 0, 0, 1),
-      //                               fontWeight: FontWeight.w700)),
-      //                     ),
-      //                     subtitle: Text(tileData[index].lasttext,
-      //                         style: GoogleFonts.inter(
-      //                             textStyle: TextStyle(
-      //                                 fontSize: 14.sp,
-      //                                 color: Color.fromRGBO(12, 16, 29, 0.6),
-      //                                 fontWeight: FontWeight.w400))),
-      //                     trailing: Padding(
-      //                       padding: EdgeInsets.only(top: 8),
-      //                       child: Column(
-      //                         children: [
-      //                           Text("${11}:${20} AM",
-      //                               style: GoogleFonts.inter(
-      //                                 textStyle: TextStyle(
-      //                                     fontSize: 10.sp,
-      //                                     color: Color.fromRGBO(0, 0, 0, 1),
-      //                                     fontWeight: FontWeight.w400),
-      //                               )),
-      //                           SizedBox(height: 3.h),
-      //                           Container(
-      //                               decoration: BoxDecoration(
-      //                                   //borderRadius: BorderRadius.circular(15),
-      //                                   border: Border.all(
-      //                                     color:
-      //                                         Color.fromRGBO(255, 202, 40, 1),
-      //                                   ),
-      //                                   shape: BoxShape.circle,
-      //                                   color: Color.fromRGBO(255, 202, 40, 1)),
-      //                               width: 22.w,
-      //                               height: 22.h,
-      //                               child: Center(
-      //                                 child: Text(
-      //                                     tileData[index].unreadMsg.toString(),
-      //                                     style: GoogleFonts.inter(
-      //                                       textStyle: TextStyle(
-      //                                           fontSize: 11.sp,
-      //                                           color:
-      //                                               Color.fromRGBO(0, 0, 0, 1),
-      //                                           fontWeight: FontWeight.w400),
-      //                                     )),
-      //                               )),
-      //                         ],
-      //                       ),
-      //                     ),
-      //                   ),
-      //                 ),
-      //               ),
-      //             );
-      //           },
-      //         ),
-      //       ),
-      // floatingActionButton: FloatingActionButton(
-      //     onPressed: () {
-      //       Navigator.push(context,
-      //           MaterialPageRoute(builder: (context) => SelectContact()));
-      //     },
-      //     backgroundColor: Color.fromRGBO(248, 206, 97, 1),
-      //     child: SvgPicture.asset("assets/icons_assets/chat_icon_floating.svg")),
-      body: getChatList(),
+
+
+      body:
+      getChatList(),
+
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SelectedContact()));
+          },
+          backgroundColor: Color.fromRGBO(248, 206, 97, 1),
+          child: SvgPicture.asset("assets/icons_assets/chat_icon_floating.svg")),
     );
   }
 
@@ -185,12 +70,12 @@ class _PingsChatViewState extends State<PingsChatView> {
   Future readData()
   async{
     String uid="plPttbFnMVPvf741ZQJ9GzkTP6V2";
-    print("Reading");
+    // print("Reading");
     // await db.collection("personal-chat-room-detail").get().then((event) {
     await db.collection("personal-chat-room-detail").where("members.$uid.isblocked").get().then((event) {
 
       for (var doc in event.docs) {
-        print("${doc.id} => ${doc.data()}");
+        // print("${doc.id} => ${doc.data()}");
       }
 
     });
@@ -218,44 +103,59 @@ class _PingsChatViewState extends State<PingsChatView> {
                 getDateTimeSinceEpoch(datetime: a.data()["timestamp"])
                     .compareTo(
                     getDateTimeSinceEpoch(datetime: b.data()["timestamp"])));
-            print(docs.toString());
-            print("LENGTH:${docs.length}");
+            // print(docs.toString());
+            // print("LENGTH:${docs.length}");
             return ListView.builder(
               itemCount: docs.length,
               itemBuilder: (context, index) {
-                if (!_isSelected.containsKey(index)) // important
-                  _isSelected[index] = false;
+                if(_isSelected[index]==0){
+
+                }
 
                 return Padding(
                   padding: const EdgeInsets.only(bottom: 2),
                   child: ListTile(
                     selected: _isSelected[index],
                     tileColor: Colors.white,
-                    selectedTileColor: Colors.amber,
+                    selectedTileColor: Color.fromRGBO(248, 206, 97, 0.31),
                     onLongPress: () {
                       setState((){
                         _isSelected[index] = !_isSelected[index];
+                        print("OP: ${_isSelected[index]}=!${_isSelected[index]} ");
                         selects = true;
+                        print("Long Press");
                       });
 
                     },
                     onTap: () {
 
-                      if (_isSelected[index]==false) {
-                        setState(() {
+                      print("Array LENGTH: ${_isSelected.length}");
+                      if(_isSelected.length==0)
+                        {
+                          selects=false;
+                        }
+                      else {
+                        if (selects == true) {
+                          setState(() {
+                            _isSelected[index] = !_isSelected[index];
+                            print("Tile Selected1");
 
-                          _isSelected.removeWhere((key, value) => true);
-                        });
-                      } else {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
+                            //_isSelected.removeWhere((key, value) => true);
+                          });
+                        }
+                        else {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
 
-                                builder: (context) => PersonalChat(state: 0,
-                                  uid: uid!,
-                                  puid: docs[index].data()["members"]["$uid"]["peeruid"])));
+                                  builder: (context) =>
+                                      PersonalChat(state: 0,
+                                          uid: uid!,
+                                          puid: docs[index]
+                                              .data()["members"]["$uid"]["peeruid"])));
+                        }
                       }
-                    },
+                      },
 
                     contentPadding: EdgeInsets.only(
                         left: 10, right: 10, top: 4, bottom: 4),
@@ -328,13 +228,27 @@ class _PingsChatViewState extends State<PingsChatView> {
           }
           else {
             return Container(
+                padding: EdgeInsets.only(top: 110),
                 child: Center(
-                    child: SingleChildScrollView(
-                      // child: Column(
-                      //   children: [lottieAnimation(emptyChatLottie), Text("Your shelves are empty !\nAdd people to start the conversation")],
-                      // ),
-                      child: Text("Empty"),
-                    )));
+                  child: Column(
+                    children: [
+                      Image.asset(
+                          "assets/tabbar_icons/tab_view_main/chats_image/emptyChat.png"),
+                      Text("No Conversation",
+                          style: GoogleFonts.raleway(
+                              textStyle: TextStyle(
+                                  color: Color.fromRGBO(0, 0, 0, 1),
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w700))),
+                      Text(
+                        "You don't made any conversation yet",
+                        style: GoogleFonts.raleway(
+                            color: Color.fromRGBO(122, 122, 122, 1),
+                            fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ));
           }
         }
             );
@@ -345,19 +259,19 @@ class _PingsChatViewState extends State<PingsChatView> {
 
   Future readChat() async{
     final db=FirebaseFirestore.instance;
-    print("CHAT DATA");
+    // print("CHAT DATA");
 
 
     await db.collection("personal-chat-room-details").get().then((event) {
     // await db.collection("personal-chat-room-detail").where("members.$uid.isblocked").get().then((event) {
       for (var doc in event.docs) {
-        print("${doc.id} => ${doc.data()}");
+        // print("${doc.id} => ${doc.data()}");
       }
     });
   }
 
   DateTime getDateTimeSinceEpoch({required String datetime}) {
-    print(DateTime.fromMillisecondsSinceEpoch(int.parse(datetime)));
+    // print(DateTime.fromMillisecondsSinceEpoch(int.parse(datetime)));
     return DateTime.fromMillisecondsSinceEpoch(int.parse(datetime));
   }
   String readTimestamp(int timestamp) {
@@ -378,7 +292,7 @@ class _PingsChatViewState extends State<PingsChatView> {
     }
 
 
-    print("Time:$time");
+    // print("Time:$time");
 
     return time;
   }
